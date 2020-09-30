d7c4e69e76dd287623fedadee24dceba0070fd90
Wess Lancaster <wess.lancaster@gmail.com>
2020-09-29 21:23:53 -0600

    Added package data, fixed some implementations, added helper methods, tested.

--- WGUPS_Objects.py
+++ WGUPS_Objects.py
@@ -1,5 +1,10 @@
-from datetime import time
+# Author:           Wesley Lancaster
+# StudentID:        #001356953
+# Date:             September 2020
+# WGUPS_Objects.py: Defines all objects and classes necessary for the project
+
 from datetime import timedelta
+
 
 # A class that represents a Truck object, with
 #   id (int)
@@ -11,14 +16,15 @@ class Truck:
     # Ctor, start time at 8
     def __init__(self, id, packages):
         self.id = id
-        self.time = timedelta(hours=8)  #start at 8 am
+        self.time = timedelta(hours=8)  # start at 8 am
         self.miles = 0.0
-        self.speed = 18.0               #miles per hour
-        self.max_packages = 16          #max amount of packages
+        self.speed = 18.0  # miles per hour
+        self.max_packages = 16  # max amount of packages
 
     @property
     def packages(self):
         return self.packages
+
     @packages.setter
     def packages(self, list):
         if list.length > self.max_packages:
@@ -37,6 +43,7 @@ class Truck:
     def to_string(self):
         return "Truck " + str(self.id) + " at " + str(self.time) + ": " + str(self.miles) + " miles"
 
+
 # A class that represents a Package needing to be delivered, with
 #   id (int)
 #   destination (int representing a location in the loc_table)
@@ -47,20 +54,122 @@ class Truck:
 #   arrival_time (time)
 #   delivery time (time)
 class Package:
-    def __init__(self, id, destination, deadline, mass, truck, pkg_group, arrival_time, delivery_time):
+    def __init__(self, id, address, city, zip, deadline, mass, status):
         self.id = id
-        self.destination = destination
-        self.deadline = deadline
+        self.address = address
+        self.city = city
+        self.zip = zip
+        self.deadline = deadline    #TODO change format
         self.mass = mass
-        self.truck = truck
-        self.pkg_group = pkg_group
-        self.arrival_time = arrival_time
+        self.status = status
+        self.loc_id = Map.lookup(address, zip)
+        self.truck = None
+        self.pkg_group = None
+    def __str__(self):
+        return "Pkg#" + str(self.id)
 
-class PackageTable:
-    def __init__(self):
-        return {
 
-        }
+# A package-specific node class to be used by the hash_table class, as a node that points to other nodes.
+class pkg_node:
+    def __init__(self, pkg):
+        self.pkg = pkg
+        self.next = None
+
+    def __str__(self):
+        return "Pkg#" + str(self.pkg.id)
+
+
+# A hash table with a given size
+class pkg_hash_table:
+    def __init__(self, size):
+        self.size = size
+        self.arr = [pkg_node(Package(-1, "", "", "", "", "", ""))]  # -1 is the sign of an empty bucket)
+        for i in range(size - 1):
+            self.arr.append(pkg_node(Package(-1, "", "", "", "", "", "")))
+
+    # Insert a new node onto the hash table
+    def insert(self, id, address, city, zip, deadline, mass, status="At Warehouse"):
+        pkg = Package(id, address, city, zip, deadline, mass, status)
+        pkg_hash = pkg.id % self.size
+        node = self.arr[pkg_hash]  # a node that will help us traverse the hash table
+        if node.pkg.id == -1:
+            node.pkg = pkg
+        else:
+            while node.next:
+                node = node.next
+            node.next = pkg_node(pkg)
+
+    # A lookup function that takes a pkg id and returns the Package object with the id; or None if none is found.
+    def lookup(self, id):
+        pkg_hash = id % self.size
+        node = self.arr[pkg_hash]   # a node that will help us traverse the hash table
+        if node.pkg.id == -1:       # if it's an empty bucket
+            return None
+        while node is not None:    # Traverse the linked list until a match is found
+            if node.pkg.id == id:
+                return node.pkg
+            node = node.next
+        return None
+
+    # Defining the string function (for easier debugging)
+    def __str__(self):
+        s = ""
+        for i in range(self.size):
+            s += str(i) + ":\t\t"
+            node = self.arr[i]
+            while node is not None:
+                s += str(node.pkg.id) + "\t"
+                node = node.next
+            s += "\n"
+        return s
+
+
+# A method that inserts package data into our hash table.
+def load_pkgs(pkgs):
+    pkgs.insert(1, "195 W Oakland Ave", "Salt Lake City", "84115", "10:30 AM", 21)
+    pkgs.insert(2, "2530 S 500 E", "Salt Lake City", "84106", "EOD", 44)
+    pkgs.insert(3, "233 Canyon Rd", "Salt Lake City", "84103", "EOD", 2)  # "Can only be on truck 2
+    pkgs.insert(4, "380 W 2880 S", "Salt Lake City", "84115", "EOD", 4)
+    pkgs.insert(5, "410 S State St", "Salt Lake City", "84111", "EOD", 5)
+    pkgs.insert(6, "3060 Lester St", "West Valley City", "84119", "10:30 AM",
+                88)  # "Delayed on flight---will not arrive to depot until 9:05 am
+    pkgs.insert(7, "1330 2100 S", "Salt Lake City", "84106", "EOD", 8)
+    pkgs.insert(8, "300 State St", "Salt Lake City", "84103", "EOD", 9)
+    pkgs.insert(9, "300 State St", "Salt Lake City", "84103", "EOD", 2)  # "Wrong address listed
+    pkgs.insert(10, "600 E 900 South", "Salt Lake City", "84105", "EOD", 1)
+    pkgs.insert(11, "2600 Taylorsville Blvd", "Salt Lake City", "84118", "EOD", 1)
+    pkgs.insert(12, "3575 W Valley Central Station bus Loop", "West Valley City", "84119", "EOD", 1)
+    pkgs.insert(13, "2010 W 500 S", "Salt Lake City", "84104", "10:30 AM", 2)
+    pkgs.insert(14, "4300 S 1300 E", "Millcreek", "84117", "10:30 AM", 88)  # "Must be delivered with 15, 19
+    pkgs.insert(15, "4580 S 2300 E", "Holladay", "84117", "9:00 AM", 4)
+    pkgs.insert(16, "4580 S 2300 E", "Holladay", "84117", "10:30 AM", 88)  # "Must be delivered with 13, 19
+    pkgs.insert(17, "3148 S 1100 W", "Salt Lake City", "84119", "EOD", 2)
+    pkgs.insert(18, "1488 4800 S", "Salt Lake City", "84123", "EOD", 6)  # "Can only be on truck 2
+    pkgs.insert(19, "177 W Price Ave", "Salt Lake City", "84115", "EOD", 37)
+    pkgs.insert(20, "3595 Main St", "Salt Lake City", "84115", "10:30 AM", 37)  # "Must be delivered with 13, 15
+    pkgs.insert(21, "3595 Main St", "Salt Lake City", "84115", "EOD", 3)
+    pkgs.insert(22, "6351 South 900 East", "Murray", "84121", "EOD", 2)
+    pkgs.insert(23, "5100 South 2700 West", "Salt Lake City", "84118", "EOD", 5)
+    pkgs.insert(24, "5025 State St", "Murray", "84107", "EOD", 7)
+    pkgs.insert(25, "5383 South 900 East #104", "Salt Lake City", "84117", "10:30 AM",
+                7)  # Delayed on flight---will not arrive to depot until 9:05 am
+    pkgs.insert(26, "5383 South 900 East #104", "Salt Lake City", "84117", "EOD", 25)
+    pkgs.insert(27, "1060 Dalton Ave S", "Salt Lake City", "84104", "EOD", 5)
+    pkgs.insert(28, "2835 Main St", "Salt Lake City", "84115", "EOD",
+                7)  # Delayed on flight---will not arrive to depot until 9:05 am
+    pkgs.insert(29, "1330 2100 S", "Salt Lake City", "84106", "10:30 AM", 2)
+    pkgs.insert(30, "300 State St", "Salt Lake City", "84103", "10:30 AM", 1)
+    pkgs.insert(31, "3365 S 900 W", "Salt Lake City", "84119", "10:30 AM", 1)
+    pkgs.insert(32, "3365 S 900 W", "Salt Lake City", "84119", "EOD",
+                1)  # Delayed on flight---will not arrive to depot until 9:05 am
+    pkgs.insert(33, "2530 S 500 E", "Salt Lake City", "84106", "EOD", 1)
+    pkgs.insert(34, "4580 S 2300 E", "Holladay", "84117", "10:30 AM", 2)
+    pkgs.insert(35, "1060 Dalton Ave S", "Salt Lake City", "84104", "EOD", 88)
+    pkgs.insert(36, "2300 Parkway Blvd", "West Valley City", "84119", "EOD", 88)  # Can only be on truck 2
+    pkgs.insert(37, "410 S State St", "Salt Lake City", "84111", "10:30 AM", 2)
+    pkgs.insert(38, "410 S State St", "Salt Lake City", "84111", "EOD", 9)  # Can only be on truck 2
+    pkgs.insert(39, "2010 W 500 S", "Salt Lake City", "84104", "EOD", 9)
+    pkgs.insert(40, "380 W 2880 S", "Salt Lake City", "84115", "10:30 AM", 45)
 
 
 # A class that represents a location to deliver a package to, with
@@ -68,47 +177,52 @@ class PackageTable:
 #   name (string)
 #   address (string)
 class Location:
-    def __init__(self, id, name, address):
+    """A location object with ID, name, and address at minimum"""
+
+    def __init__(self, id, name, address, zip):
         self.id = id
         self.name = name
         self.address = address
+        self.zip = zip
+
     def to_string(self):
         return self.id + "\t" + self.name + "\t" + self.address
 
+
 # A class containing a hash table of all locations, as well as an adjacency matrix
 class Map:
+    locations = {
+        Location(0, "Western Governors University",
+                 "4001 South 700 East, Salt Lake City, UT", "84107"),
+        Location(1, "International Peace Gardens", "1060 Dalton Ave S", "84104"),
+        Location(2, "Sugar House Park", "1330 2100 S", "84106"),
+        Location(3, "Taylorsville-Bennion Heritage City Gov Off", "1488 4800 S", "84123"),
+        Location(4, "Salt Lake City Division of Health Services", "177 W Price Ave", "84115"),
+        Location(5, "South Salt Lake Public Works", "195 W Oakland Ave", "84115"),
+        Location(6, "Salt Lake City Streets and Sanitation", "2010 W 500 S", "84104"),
+        Location(7, "Deker Lake", "2300 Parkway Blvd", "84119"),
+        Location(8, "Salt Lake City Ottinger Hall", "233 Canyon Rd", "84103"),
+        Location(9, "Columbus Library", "2530 S 500 E", "84106"),
+        Location(10, "Taylorsville City Hall", "2600 Taylorsville Blvd", "84118"),
+        Location(11, "South Salt Lake Police", "2835 Main St", "84115"),
+        Location(12, "Council Hall", "300 State St", "84103"),
+        Location(13, "Redwood Park", "3060 Lester St", "84119"),
+        Location(14, "Salt Lake County Mental Health", "3148 S 1100 W", "84119"),
+        Location(15, "Salt Lake County/United Police Dept", "3365 S 900 W", "84119"),
+        Location(16, "West Valley Prosecutor",
+                 "3575 W Valley Central Station bus Loop", "84119"),
+        Location(17, "Housing Auth. of Salt Lake County", "3595 Main St", "84115"),
+        Location(18, "Utah DMV Administrative Office", "380 W 2880 S", "84115"),
+        Location(19, "Third District Juvenile Court", "410 S State St", "84111"),
+        Location(20, "Cottonwood Regional Softball Complex", "4300 S 1300 E", "84117"),
+        Location(21, "Holiday City Office", "4580 S 2300 E", "84117"),
+        Location(22, "Murray City Museum", "5025 State St", "84107"),
+        Location(23, "Valley Regional Softball Complex", "5100 South 2700 West", "84118"),
+        Location(24, "City Center of Rock Springs", "5383 S 900 East #104", "84117"),
+        Location(25, "Rice Terrace Pavilion Park", "600 E 900 South", "84105"),
+        Location(26, "Wheeler Historic Farm", "6351 South 900 East", "84121")
+    }
     def __init__(self):
-        locations ={
-            Location( 0, "Western Governors University",
-                      "4001 South 700 East, Salt Lake City, UT 84107"),
-            Location( 1, "International Peace Gardens",                 "1060 Dalton Ave S 84104"),
-            Location( 2, "Sugar House Park",                            "1330 2100 S 84106"),
-            Location( 3, "Taylorsville-Bennion Heritage City Gov Off",  "1488 4800 S 84123"),
-            Location( 4, "Salt Lake City Division of Health Services",  "177 W Price Ave 84115"),
-            Location( 5, "South Salt Lake Public Works",                "195 W Oakland Ave 84115"),
-            Location( 6, "Salt Lake City Streets and Sanitation",       "2010 W 500 S 84104"),
-            Location( 7, "Deker Lake",                                  "2300 Parkway Blvd 84119"),
-            Location( 8, "Salt Lake City Ottinger Hall",                "233 Canyon Rd 84103"),
-            Location( 9, "Columbus Library",	                        "2530 S 500 E 84106"),
-            Location(10, "Taylorsville City Hall",                      "2600 Taylorsville Blvd 84118"),
-            Location(11, "South Salt Lake Police",                      "2835 Main St 84115"),
-            Location(12, "Council Hall",               	                "300 State St(84103)"),
-            Location(13, "Redwood Park",             	                "3060 Lester St(84119)"),
-            Location(14, "Salt Lake County Mental Health",	            "3148 S 1100 W(84119)"),
-            Location(15, "Salt Lake County/United Police Dept",	        "3365 S 900 W(84119)"),
-            Location(16, "West Valley Prosecutor",
-                     "3575 W Valley Central Station bus Loop(84119)"),
-            Location(17, "Housing Auth. of Salt Lake County",	        "3595 Main St(84115)"),
-            Location(18, "Utah DMV Administrative Office",	            "380 W 2880 S(84115)"),
-            Location(19, "Third District Juvenile Court",	            "410 S State St(84111)"),
-            Location(20, "Cottonwood Regional Softball Complex",	    "4300 S 1300 E(84117)"),
-            Location(21, "Holiday City Office",	                        "4580 S 2300 E(84117)"),
-            Location(22, "Murray City Museum",	                        "5025 State St(84107)"),
-            Location(23, "Valley Regional Softball Complex",	        "5100 South 2700 West(84118)"),
-            Location(24, "City Center of Rock Springs",	                "5383 S 900 East #104(84117)"),
-            Location(25, "Rice Terrace Pavilion Park",	                "600 E 900 South(84105)"),
-            Location(26, "Wheeler Historic Farm",	                    "6351 South 900 East(84121)")
-        }
         self.distances = {
             {0},
             {7.2, 0},
@@ -144,8 +258,17 @@ class Map:
             {3.6, 13.0, 7.4, 10.1, 5.5, 7.2, 14.2, 10.7, 14.1, 6.0, 6.8, 6.4, 14.1, 10.5, 8.8, 8.4, 13.6, 5.2, 6.9,
              13.1, 4.1, 4.7, 3.1, 7.8, 1.3, 8.3, 0}
         }
+
     @property
     def distances(self, x, y):
         if x > y:
             return self.distances[y][x]
         return self.distances[x][y]
+
+    # Looks up address and zip for a match, then returns a location id if a match is found
+    @staticmethod
+    def lookup(address, zip):
+        for l in Map.locations:
+            if l.address == address and l.zip == zip:
+                return l.id
+        return -1
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-# Author: Wesley Lancaster
-# Date:   September 2020
+# Author:       Wesley Lancaster
+# StudentID:    #001356953
+# Date:         September 2020
+# main.py:      Facilitates user interaction, and holds all instance data
 
 # The Western Governors University Parcel Service (WGUPS) needs to determine the best route and delivery distribution
 # for their Daily Local Deliveries. The Salt Lake City DLD route has two trucks & drivers, and an average of 40
@@ -14,19 +16,68 @@
 # which packages are at the hub, and which are en route. The intent is to use this program for this specific location
 # and to use the same program in different cities as WGUPS expands its business.
 
-from WGUPS_Objects import Truck, Package, PackageTable, Location, Map
+from WGUPS_Objects import Truck, Package, Location, Map, pkg_hash_table, load_pkgs
+from datetime import timedelta
 
-# MAIN CLASS: Facilitates user interaction, and holds all instance data
-# Instantiate all variables
-trucks = [Truck(1, list()), Truck(2, {})]
-# TODO packages = PackageTable()
 
-# ***** The Delivery Algorithm ***** #
+# Reorder function: The essential algorithm of the project. Called by the simulate function, defines the routes that
+# trucks will take to deliver packages, and returns to the simulation.
+def reorder(trucks, packages):
+    return  # TODO delete
+
+
+# * * * * *   Simulate Function   * * * * * #
+# Simulates the WGUPS workday, printing package status updates between 2 given times. This function
+# is the "clock" of the simulation, its only logic is to make sure the timeline is correct. Other functions are used for
+# the actual algorithm
+def simulate(status_time):
+    print("\n\n\nPrint statuses at ", status_time)
+    # Instantiate all variables
+    trucks = [Truck(1, list()), Truck(2, {})]
+    pkgs = pkg_hash_table(16)
+    load_pkgs(pkgs)
+    print(pkgs)
+
+    # Wait for user to continue
+    print("Press enter to continue...", end='')
+    input()
+    print("\n\n")
+
+
+# * * * * *   The Delivery Algorithm   * * * * * #
 # TODO Check if there are (or will be?) packages not loaded on a truck
 # TODO If so, plan on delivering early-ETA packages first, then fill in the time gaps with as many packages as possible
 
-# TODO Temp testing
-trucks[0].drive(2.0)
-trucks[1].drive(3.55)
-print(trucks[0].to_string())
-print(trucks[1].to_string())
+
+# * * * * *   Main Menu   * * * * * #
+# (Project Requirement): Provide an interface for the insert and look-up functions to view the status of any
+# package at any time. This function should return all information about each package, including delivery status.
+#   1.  Provide screenshots to show package status of all packages at a time between 8:35 a.m. and 9:25 a.m.
+#   2.  Provide screenshots to show package status of all packages at a time between 9:35 a.m. and 10:25 a.m.
+#   3.  Provide screenshots to show package status of all packages at a time between 12:03 p.m. and 1:12 p.m.
+selection = -1
+while selection != 0:
+    print("* * * WGUPS Simulator * * *\n"
+          "Please make a selection:\n"
+          "\t1. Run full simulation\n"
+          "\t2. Show package statuses at a time\n"
+          "\t0. Exit")
+    try:
+        selection = 1  # TODO int(input())
+    except:
+        print("\n\n\nBad choice, try again")
+        continue
+
+    if selection == 1:
+        simulate(timedelta(hours=0))
+    elif selection == 2:
+        print("Please input the time by hour and minute.\n"
+              "Hour:\t", end='')
+        hour = int(input())
+        print("Minute:\t", end='')
+        minute = int(input())
+        simulate(timedelta(hours=hour, minutes=minute))
+    elif selection == 2:
+        simulate(timedelta(hours=12, minutes=3), timedelta(hours=13, minutes=12))
+    else:
+        continue
